9059cd48a9d2f048839098043d6d2b56a125b666
DRMcLaughlin <dougrmclaughlin@gmail.com>
2025-04-13 14:26:18 -0500

    G: Added player projectile functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from circleshape import CircleShape
 from player import Player # Imports Player Class
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 def main():
 	# Initialize pygame
 	pygame.init()
@@ -21,13 +22,14 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
-
+	shots = pygame.sprite.Group()
 
 
 	#Set Player's containers to add instances to both groups
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = updatable
+	Shot.containers = (shots, updatable, drawable)
 
 	# Instantiate the Asteroid Field
 	asteroid_field = AsteroidField()
@@ -43,12 +45,12 @@ def main():
 				return
 
 		updatable.update(dt)
-		
+
 		for asteroid in asteroids:
 			if player.check_collision(asteroid):
 				print("Game over!")
 				import sys
-				sys.exit()		
+				sys.exit()
 
 		# Clear the screen
 		screen.fill("black")
